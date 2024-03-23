422d11ed5021df8032255ce69b018bc133c923f8
Allie <alliesafronova@gmail.com>
2024-03-23 14:58:58 +0700

    13 planets

--- universe.sql
+++ universe.sql
@@ -6,23 +6,14 @@
 -- Dumped by pg_dump version 14.11 (Ubuntu 14.11-0ubuntu0.22.04.1)
 
 SET statement_timeout = 0;
-
 SET lock_timeout = 0;
-
 SET idle_in_transaction_session_timeout = 0;
-
 SET client_encoding = 'UTF8';
-
 SET standard_conforming_strings = on;
-
-SELECT pg_catalog.set_config ('search_path', '', false);
-
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
-
 SET xmloption = content;
-
 SET client_min_messages = warning;
-
 SET row_security = off;
 
 DROP DATABASE universe;
@@ -30,32 +21,22 @@ DROP DATABASE universe;
 -- Name: universe; Type: DATABASE; Schema: -; Owner: allie
 --
 
-CREATE DATABASE universe
-WITH
-    TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.UTF-8';
+CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.UTF-8';
+
 
 ALTER DATABASE universe OWNER TO allie;
 
-\connect universe 
+\connect universe
 
 SET statement_timeout = 0;
-
 SET lock_timeout = 0;
-
 SET idle_in_transaction_session_timeout = 0;
-
 SET client_encoding = 'UTF8';
-
 SET standard_conforming_strings = on;
-
-SELECT pg_catalog.set_config ('search_path', '', false);
-
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
-
 SET xmloption = content;
-
 SET client_min_messages = warning;
-
 SET row_security = off;
 
 SET default_tablespace = '';
@@ -67,8 +48,11 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.galaxy (
-    galaxy_id integer NOT NULL, name character varying(40), shape character varying(30)
+    galaxy_id integer NOT NULL,
+    name character varying(40),
+    shape character varying(30)
 );
+
 
 ALTER TABLE public.galaxy OWNER TO allie;
 
@@ -76,9 +60,14 @@ ALTER TABLE public.galaxy OWNER TO allie;
 -- Name: galaxy_galaxy_id_seq; Type: SEQUENCE; Schema: public; Owner: allie
 --
 
-CREATE SEQUENCE public.galaxy_galaxy_id_seq AS integer START
-WITH
-    1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE SEQUENCE public.galaxy_galaxy_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 
 ALTER TABLE public.galaxy_galaxy_id_seq OWNER TO allie;
 
@@ -88,13 +77,20 @@ ALTER TABLE public.galaxy_galaxy_id_seq OWNER TO allie;
 
 ALTER SEQUENCE public.galaxy_galaxy_id_seq OWNED BY public.galaxy.galaxy_id;
 
+
 --
 -- Name: planet; Type: TABLE; Schema: public; Owner: allie
 --
 
 CREATE TABLE public.planet (
-    planet_id integer NOT NULL, name character varying(30), description text, year_in_earth_days numeric(10, 2), has_moons boolean, star_id integer
+    planet_id integer NOT NULL,
+    name character varying(30),
+    description text,
+    year_in_earth_days numeric(10,2),
+    has_moons boolean,
+    star_id integer
 );
+
 
 ALTER TABLE public.planet OWNER TO allie;
 
@@ -102,9 +98,14 @@ ALTER TABLE public.planet OWNER TO allie;
 -- Name: planet_planet_id_seq; Type: SEQUENCE; Schema: public; Owner: allie
 --
 
-CREATE SEQUENCE public.planet_planet_id_seq AS integer START
-WITH
-    1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE SEQUENCE public.planet_planet_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 
 ALTER TABLE public.planet_planet_id_seq OWNER TO allie;
 
@@ -114,13 +115,18 @@ ALTER TABLE public.planet_planet_id_seq OWNER TO allie;
 
 ALTER SEQUENCE public.planet_planet_id_seq OWNED BY public.planet.planet_id;
 
+
 --
 -- Name: star; Type: TABLE; Schema: public; Owner: allie
 --
 
 CREATE TABLE public.star (
-    star_id integer NOT NULL, name character varying(30), radius_in_km integer, galaxy_id integer
+    star_id integer NOT NULL,
+    name character varying(30),
+    radius_in_km integer,
+    galaxy_id integer
 );
+
 
 ALTER TABLE public.star OWNER TO allie;
 
@@ -128,9 +134,14 @@ ALTER TABLE public.star OWNER TO allie;
 -- Name: star_star_id_seq; Type: SEQUENCE; Schema: public; Owner: allie
 --
 
-CREATE SEQUENCE public.star_star_id_seq AS integer START
-WITH
-    1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE SEQUENCE public.star_star_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 
 ALTER TABLE public.star_star_id_seq OWNER TO allie;
 
@@ -140,11 +151,13 @@ ALTER TABLE public.star_star_id_seq OWNER TO allie;
 
 ALTER SEQUENCE public.star_star_id_seq OWNED BY public.star.star_id;
 
+
 --
 -- Name: galaxy galaxy_id; Type: DEFAULT; Schema: public; Owner: allie
 --
 
 ALTER TABLE ONLY public.galaxy ALTER COLUMN galaxy_id SET DEFAULT nextval('public.galaxy_galaxy_id_seq'::regclass);
+
 
 --
 -- Name: planet planet_id; Type: DEFAULT; Schema: public; Owner: allie
@@ -152,162 +165,119 @@ ALTER TABLE ONLY public.galaxy ALTER COLUMN galaxy_id SET DEFAULT nextval('publi
 
 ALTER TABLE ONLY public.planet ALTER COLUMN planet_id SET DEFAULT nextval('public.planet_planet_id_seq'::regclass);
 
+
 --
 -- Name: star star_id; Type: DEFAULT; Schema: public; Owner: allie
 --
 
 ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.star_star_id_seq'::regclass);
 
+
 --
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: allie
 --
 
-INSERT INTO public.galaxy VALUES ( 1, 'Milky Way', 'Barred spiral' );
-
-INSERT INTO public.galaxy VALUES ( 2, 'Messier 109', 'Barred Spiral' );
-
+INSERT INTO public.galaxy VALUES (1, 'Milky Way', 'Barred spiral');
+INSERT INTO public.galaxy VALUES (2, 'Messier 109', 'Barred Spiral');
 INSERT INTO public.galaxy VALUES (3, 'Andromeda', 'Spiral');
-
 INSERT INTO public.galaxy VALUES (4, 'Pinwheel', 'Spiral');
-
 INSERT INTO public.galaxy VALUES (5, 'Triangulum', 'Spiral');
-
 INSERT INTO public.galaxy VALUES (6, 'Sunflower', 'Spiral');
-
 INSERT INTO public.galaxy VALUES (7, 'Sombrero', 'Spiral');
-
 INSERT INTO public.galaxy VALUES (8, 'IC 1101', 'Elliptical');
-
 INSERT INTO public.galaxy VALUES (9, 'Messier 59', 'Elliptical');
-
 INSERT INTO public.galaxy VALUES (10, 'Maffei 1', 'Elliptical');
-
-INSERT INTO
-    public.galaxy
-VALUES (
-        11, 'Small Magellanic Cloud', 'Irregular'
-    );
-
+INSERT INTO public.galaxy VALUES (11, 'Small Magellanic Cloud', 'Irregular');
 INSERT INTO public.galaxy VALUES (12, 'NGC 5477', 'Irregular');
-
 INSERT INTO public.galaxy VALUES (13, 'IC 4710', 'Irregular');
-
 INSERT INTO public.galaxy VALUES (14, 'NGC 4866', 'Lenticular');
+
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: allie
 --
 
-INSERT INTO
-    public.planet
-VALUES (
-        1, 'Mercury', 'It is the smallest planet in the solar system and it has more craters and impact marks than any other planet. ', 88.00, false, 1
-    );
+INSERT INTO public.planet VALUES (1, 'Mercury', 'It is the smallest planet in the solar system and it has more craters and impact marks than any other planet. ', 88.00, false, 1);
+INSERT INTO public.planet VALUES (2, 'Venus', 'It is the hottest planet in the solar system and it rotates in the opposite direction to other planets.', 225.00, false, 1);
+INSERT INTO public.planet VALUES (3, 'Earth', 'It is the only known planet to support life.', 365.25, false, 1);
+INSERT INTO public.planet VALUES (4, 'Mars', 'It is the only other planet besides Earth that has polar ice caps and it experiences huge dust storms - the largest in the solar system.', 687.00, true, 1);
+INSERT INTO public.planet VALUES (5, 'Jupiter', 'It is the largest and most massive planet in the solar system.', 4333.00, true, 1);
+INSERT INTO public.planet VALUES (6, 'Saturn', 'It is the least dense planet in the solar system and it has the fastest winds of any other planet.', 10759.00, true, 1);
+INSERT INTO public.planet VALUES (7, 'Uranus', 'It is the coldest planet in the solar system', 84.00, true, 1);
+INSERT INTO public.planet VALUES (8, 'Neptune', 'It is the outermost planet in the solar system', 60182.00, true, 1);
+INSERT INTO public.planet VALUES (13, 'Makemake', 'It is the closest dwarf planet to the Sun. It lies on the main asteroid belt, between the planets Mars and Jupiter.', 1682.00, false, 1);
+INSERT INTO public.planet VALUES (12, 'Makemake', 'It lies beyond the orbit of Neptune and ts discovery gave rise to the dwarf planet group.', 112785.00, false, 1);
+INSERT INTO public.planet VALUES (11, 'Haumea', 'It is located beyond the orbit of Neptune and has a unique egg-like shape because of its fast rotation.', 103960.00, true, 1);
+INSERT INTO public.planet VALUES (10, 'Eris', 'It is the furthest dwarf planet from the Sun, and is also the most massive dwarf planet. It is located beyond the orbit of Neptune.', 203390.00, true, 1);
+INSERT INTO public.planet VALUES (9, 'Pluto', 'It is te largest dwarf planet and its orbit is chaotic and unpredictable.', 90582.00, true, 1);
 
-INSERT INTO
-    public.planet
-VALUES (
-        2, 'Venus', 'It is the hottest planet in the solar system and it rotates in the opposite direction to other planets.', 225.00, false, 1
-    );
-
-INSERT INTO
-    public.planet
-VALUES (
-        3, 'Earth', 'It is the only known planet to support life.', 365.25, false, 1
-    );
-
-INSERT INTO
-    public.planet
-VALUES (
-        4, 'Mars', 'It is the only other planet besides Earth that has polar ice caps and it experiences huge dust storms - the largest in the solar system.', 687.00, true, 1
-    );
-
-INSERT INTO
-    public.planet
-VALUES (
-        5, 'Jupiter', 'It is the largest and most massive planet in the solar system.', 4333.00, true, 1
-    );
-
-INSERT INTO
-    public.planet
-VALUES (
-        6, 'Saturn', 'It is the least dense planet in the solar system and it has the fastest winds of any other planet.', 10759.00, true, 1
-    );
-
-INSERT INTO
-    public.planet
-VALUES (
-        7, 'Uranus', 'It is the coldest planet in the solar system', 84.00, true, 1
-    );
-
-INSERT INTO
-    public.planet
-VALUES (
-        8, 'Neptune', 'It is the outermost planet in the solar system', 60182.00, true, 1
-    );
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: allie
 --
 
 INSERT INTO public.star VALUES (1, 'Sun', 696000, 1);
-
 INSERT INTO public.star VALUES (2, 'Betelgeuse', 822480000, 1);
-
 INSERT INTO public.star VALUES (3, 'Aldebaran', 30739200, 1);
-
 INSERT INTO public.star VALUES (4, 'Andromeda I', 6160000, 3);
-
 INSERT INTO public.star VALUES (5, 'M33-1', 7200000, 5);
-
 INSERT INTO public.star VALUES (6, 'SMC-1', 5000000, 11);
+
 
 --
 -- Name: galaxy_galaxy_id_seq; Type: SEQUENCE SET; Schema: public; Owner: allie
 --
 
-SELECT pg_catalog.setval ( 'public.galaxy_galaxy_id_seq', 14, true );
+SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 14, true);
+
 
 --
 -- Name: planet_planet_id_seq; Type: SEQUENCE SET; Schema: public; Owner: allie
 --
 
-SELECT pg_catalog.setval ( 'public.planet_planet_id_seq', 8, true );
+SELECT pg_catalog.setval('public.planet_planet_id_seq', 13, true);
+
 
 --
 -- Name: star_star_id_seq; Type: SEQUENCE SET; Schema: public; Owner: allie
 --
 
-SELECT pg_catalog.setval ( 'public.star_star_id_seq', 6, true );
+SELECT pg_catalog.setval('public.star_star_id_seq', 6, true);
+
 
 --
 -- Name: galaxy galaxy_pkey; Type: CONSTRAINT; Schema: public; Owner: allie
 --
 
 ALTER TABLE ONLY public.galaxy
-ADD CONSTRAINT galaxy_pkey PRIMARY KEY (galaxy_id);
+    ADD CONSTRAINT galaxy_pkey PRIMARY KEY (galaxy_id);
+
 
 --
 -- Name: planet planet_pkey; Type: CONSTRAINT; Schema: public; Owner: allie
 --
 
 ALTER TABLE ONLY public.planet
-ADD CONSTRAINT planet_pkey PRIMARY KEY (planet_id);
+    ADD CONSTRAINT planet_pkey PRIMARY KEY (planet_id);
+
 
 --
 -- Name: star star_pkey; Type: CONSTRAINT; Schema: public; Owner: allie
 --
 
 ALTER TABLE ONLY public.star
-ADD CONSTRAINT star_pkey PRIMARY KEY (star_id);
+    ADD CONSTRAINT star_pkey PRIMARY KEY (star_id);
+
 
 --
 -- Name: star star_galaxy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: allie
 --
 
 ALTER TABLE ONLY public.star
-ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES public.galaxy (galaxy_id);
+    ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
+
 
 --
 -- PostgreSQL database dump complete
 --
+
